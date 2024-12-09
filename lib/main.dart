a9734d8d4794fe8f7f4bbd51bcad383886169653
Bastian2312 <bastian.adiputras@gmail.com>
2024-12-09 14:13:43 +0700

    added authentication without admin

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
+import 'package:bali_heritage/authentication/login.dart';
 import 'package:flutter/material.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:provider/provider.dart';
-import 'homepage/homepage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,11 +22,14 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           useMaterial3: true,
           colorScheme: ColorScheme.fromSwatch(
-            primarySwatch: Colors.deepPurple,
-          ).copyWith(secondary: Colors.deepPurple[400]),
+            primarySwatch: Colors.orange,
+          ).copyWith(
+            secondary: Colors.orange[400],
+            background: Colors.white,
+          ),
         ),
-        home: const HomePage(), // Use the correct widget
-      ),
+        home: const LoginPage(),
+      ), // Removed the misplaced semicolon here.
     );
   }
 }
